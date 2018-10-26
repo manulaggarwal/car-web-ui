03fd999802779c5d6a611829a62c8f015b3bfc2a
Manul Aggarwal <manulaggarwal@Manuls-MacBook-Air.local>
2018-10-26 04:10:38 +0200

    Updates

---
+++ precache-manifest.a6504c051af498e2de4a1276b2fa3df9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d2254b494c305c4a5d53685e9b8929a",
+    "url": "/car-web-ui/static/media/car4.1d2254b4.png"
+  },
+  {
+    "revision": "6e9eda1821b0f9b039f88183ae694f08",
+    "url": "/car-web-ui/static/media/car3.6e9eda18.png"
+  },
+  {
+    "revision": "a504d6ccb83eb9b36e2b868cf04a085b",
+    "url": "/car-web-ui/static/media/bw-car.a504d6cc.png"
+  },
+  {
+    "revision": "660507fe373b455ef26c",
+    "url": "/car-web-ui/static/js/runtime~main.660507fe.js"
+  },
+  {
+    "revision": "2de9de953a45f7eca6c4",
+    "url": "/car-web-ui/static/js/main.2de9de95.chunk.js"
+  },
+  {
+    "revision": "145eb21de9e3f11be773",
+    "url": "/car-web-ui/static/js/1.145eb21d.chunk.js"
+  },
+  {
+    "revision": "2de9de953a45f7eca6c4",
+    "url": "/car-web-ui/static/css/main.c3e28d06.chunk.css"
+  },
+  {
+    "revision": "9699752c7b75a87bf73b2b4d066b8043",
+    "url": "/car-web-ui/index.html"
+  }
+];

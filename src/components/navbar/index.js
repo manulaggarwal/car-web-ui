d30626f296318fe32d64ebefc5cbe94880417efb
Manul Aggarwal <manulaggarwal@Manuls-MacBook-Air.local>
2018-10-26 04:04:35 +0200

    refactoring.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react'
 import './navbar.css'
 
-import {Navbar, Nav, NavDropdown, MenuItem} from 'react-bootstrap'
+import {NavDropdown, MenuItem} from 'react-bootstrap'
 
 class HeaderNavbar extends Component {
     render() {
         return (
             <div className="navbar-container">
-                        <NavDropdown eventKey={1} title="&#9776;" >
-                            <MenuItem eventKey={1.1}>How It Works?</MenuItem>
-                            <MenuItem eventKey={1.2}>Services</MenuItem>
-                        </NavDropdown>
-                   
+                <NavDropdown eventKey={1} title="&#9776;" >
+                    <MenuItem eventKey={1.1}>How It Works?</MenuItem>
+                    <MenuItem eventKey={1.2}>Services</MenuItem>
+                </NavDropdown>
             </div>
         )
     }
